a3bc1100efdecb5cf3e0d9d5673170829b1f4cb9
Kowsik002 <kowsiknarumanchi577@gmail.com>
2024-10-02 12:14:27 +0530

    dk

--- capturing/script.js
+++ capturing/script.js
@@ -3,3 +3,21 @@ let submenu = document.getElementById("sub-menu");
 let button = document.getElementById("button");
 
 //toggle button
+// Open and close the submenu when toggle button is clicked
+button.addEventListener("click", () => {
+  submenu.style.display = submenu.style.display === "none" ? "block" : "none";
+});
+
+//event capturing
+// Use event capturing to detect a click on the parent navMenu element
+document.addEventListener(
+  "click",
+  function (e) {
+    // Check if the click occurred outside the navigation menu
+    if (!navmenu.contains(e.target)) {
+      submenu.style.display = "none";
+      console.log("Click detected outside the menu, closing sub menu.");
+    }
+  },
+  true
+);
